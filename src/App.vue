c691804c03686fe5b5d82ef32e7b275a0f1fe497
rivalcoba <ivan.rivalcoba@gmail.com>
2022-07-08 17:12:03 -0500

    Recolectando los datos

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import TheHeader from "./components/TheHeader.vue";
+// App State
+import loadingImage from "./assets/loading.gif";
+import { reactive } from "vue";
+console.log(loadingImage);
+
+let state = reactive({
+  loading: true,
+  title: "Global",
+  dataDate: "",
+  stats: {},
+  countries: [],
+});
+// Metodo para la recolección de datos
+async function fetchCovidData() {
+  const res = await fetch("https://api.covid19api.com/summary");
+  const data = await res.json();
+  return data;
+}
+(async () => {
+  const covidSummary = await fetchCovidData();
+  state.dataDate = covidSummary.Date;
+  state.stats = covidSummary.Global;
+  state.countries = covidSummary.countries;
+  state.loading = false;
+})();
 </script>
 
 <template>
-  <div class="container">
-    <!-- COVID TRACKER -->
-    <TheHeader></TheHeader>
+  <!-- App Header -->
+  <TheHeader />
+  <main 
+    class="text-center" v-if="!state.loading">
+    Show Data
+  </main>
+  <main 
+    class="flex flex-col align-center justify-center text-center" v-else>
+  <div class="text-gray-500 text-3xl mt-10 mb-6">
+    Fetching Data
   </div>
+  <img 
+    src="./assets/loading.gif" 
+    alt="Loading Image"
+    class="w-24 m-auto">
+  </main>
 </template>
