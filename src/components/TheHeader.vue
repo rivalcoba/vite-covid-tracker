58a77d44090eb27ad8c979fc677bcd550572b9c7
rivalcoba <ivan.rivalcoba@gmail.com>
2022-07-08 00:22:57 -0500

    Header cCompleto

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,22 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <p>
+    <div class="text-3xl md:text-5xl font-bold mb-3">
       Estadisticas COVID-19 🦠
+    </div>
+    <p>
+      API por
+      <a 
+      class="text-blue-300" 
+      href="https://covid19api.com"
+      target="_blank">
+        covid19api.com
+      </a>
     </p>
   </header>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
-
 </style>
