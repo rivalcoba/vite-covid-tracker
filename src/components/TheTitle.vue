280e0ed43c95ef4f9c39e48b35617145ba25e0d6
rivalcoba <ivan.rivalcoba@gmail.com>
2022-07-08 20:00:30 -0500

    Formateando la fecha

--- src/components/TheTitle.vue
+++ src/components/TheTitle.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="text-center">
-    <h2 class="text-3xl font-bold">{{title}}</h2>
+    <h2 class="text-3xl font-bold">{{ title }}</h2>
     <div class="text-2xl mt-4 mb-10">
-      {{dataDate}}
+      {{ timestamp }}
     </div>
   </div>
 </template>
 
 <script setup>
+// Importando momentJs
+import moment from "moment";
+// Importando prop
+import { computed } from "vue";
 // Definiendo Propiedades
-defineProps(['title','dataDate']);
+const props = defineProps(["title", "dataDate"]);
+// Creando propiedades
+const timestamp = computed(() =>
+  moment(props.dataDate).format("MMMM DD YYYY, hh:mm:ss a")
+);
 </script>
