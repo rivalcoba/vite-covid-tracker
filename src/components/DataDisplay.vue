3f2d3eefc527dd33cd04e7e57c50328e541beb8f
rivalcoba <ivan.rivalcoba@gmail.com>
2022-07-10 13:29:39 -0500

    Cajas en DataDisplay

--- src/components/DataDisplay.vue
+++ src/components/DataDisplay.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="grid md:grid-cols-2 gap-4">
+    <!-- BOX 1 -->
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
       <h3 class="text-3xl text-blue-900 font-bold mb-4">
         Casos
@@ -7,6 +8,24 @@
       <div class="text-2xl mb-4">
         <span class="font-bold">Nuevos:</span>
         {{ data.NewConfirmed }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Totales:</span>
+        {{ data.TotalConfirmed }}
+      </div>
+    </div>
+    <!-- BOX 2 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        Fallecimientos
+      </h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Nuevos:</span>
+        {{ data.NewDeaths }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Totales:</span>
+        {{ data.TotalDeaths }}
       </div>
     </div>
   </div>
